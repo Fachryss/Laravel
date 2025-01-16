79c7b95c3efa127a360df6f74404d3aaef378265
Fachryss <aadyusuf18@gmail.com>
2025-01-16 07:49:51 +0700

    first commit

---
+++ app/Models/Transaction_detail.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction_detail extends Model
+{
+    use HasFactory;
+    use HasFactory;
+    protected $table = 'transaction_detail';
+    protected $fillable=[
+        "transaction_id",
+        "movie_id",
+        "quantity",
+    ];
+    public $timestamps = true;
+}
